497ff016f47f346e7b7b68dd48b317ab3651a845
BOPPANA VENKATESH <boppanavenkatesh97@gmail.com>
2023-03-24 12:25:03 +0530

    Updated(1)

--- src/main/java/org/example/DimensionalProcessing.java
+++ src/main/java/org/example/DimensionalProcessing.java
@@ -5,6 +5,10 @@ import java.sql.*;
 import javax.xml.parsers.*;
 import org.w3c.dom.*;
 public class DimensionalProcessing {
+    String url = "jdbc:mysql://localhost:3306/DataModeling?sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false&createDatabaseIfNotExist=true";
+    String username = "root"; // replace with your username
+    String password = "KVrsmck@21"; // replace with your password
+    String metaTable = "DimensionTables";
     public DimensionalProcessing(){}
     public boolean CreateMetaDT(){
         String url = "jdbc:mysql://localhost:3306/DataModeling";
@@ -25,11 +29,8 @@ public class DimensionalProcessing {
             return false;
         }
     }
-    public boolean GenerateDTs(String xmlFile){
-        String url = "jdbc:mysql://localhost:3306/DataModeling";
-        String username = "root"; // replace with your username
-        String password = "KVrsmck@21"; // replace with your password
-        String metaTable = "DimensionTables";
+    public boolean GenerateDTs(String workingDir, String xmlFileName){
+        String xmlFile = workingDir.concat(xmlFileName);
         try {
             // Read the metadata from the XML file
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -81,12 +82,61 @@ public class DimensionalProcessing {
                 } catch (SQLException e) {
                     System.out.println(sql+" "+e.getMessage());
                 }
-
+                System.out.println("*** Populating Tables Started ***");
+                DimensionalProcessing DimProc = new DimensionalProcessing();
+                if (DimProc.PopulateDTs(workingDir, DimensionTableNames[i], DimensionVariableNames, DimensionVariableTypes)){
+                    System.out.println("Operation Success ...");
+                }
+                else {
+                    System.out.println("Operation Failed ...");
+                }
             }
             System.out.println("[Build Ended ...]");
             return true;
         } catch (Exception e) {
             e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean PopulateDTs(String workingDir, String csvFileName, String[] columnNames, String[] columnTypes) throws IOException {
+        String csvFile = workingDir.concat("/");
+        csvFile += csvFileName;
+        csvFile += ".csv";
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stmt = conn.createStatement()) {
+            // Read the data from the CSV file and insert it into MySQL
+            BufferedReader br = new BufferedReader(new FileReader(csvFile));
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                String insertSql = "INSERT INTO " + csvFileName + " (";
+                for (int i = 0; i < columnNames.length; i++) {
+                    insertSql += columnNames[i];
+                    if (i < columnNames.length - 1) {
+                        insertSql += ", ";
+                    }
+                }
+                insertSql += ") VALUES (";
+                for (int i = 0; i < values.length; i++) {
+                    if (columnTypes[i].equals("VARCHAR(255)")) {
+                        insertSql += "'" + values[i] + "'";
+                    } else {
+                        insertSql += values[i];
+                    }
+                    if (i < values.length - 1) {
+                        insertSql += ", ";
+                    }
+                }
+                insertSql += ")";
+                stmt.executeUpdate(insertSql);
+                System.out.println(insertSql+" Executed Successfully...");
+            }
+            br.close();
+            System.out.println("For Table: "+ csvFileName +", Values Populated Successfully...");
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
             return false;
         }
     }
--- src/main/java/org/example/Driver.java
+++ src/main/java/org/example/Driver.java
@@ -4,7 +4,8 @@ public class Driver {
     public static void main(String[] args){
         DimensionalProcessing Dimproc = new DimensionalProcessing();
         Dimproc.CreateMetaDT();
-        String xmlFilePath = "/Users/boppanavenkatesh/Desktop/XML Practice/DM Instance.xml";
-        Dimproc.GenerateDTs(xmlFilePath);
+        String workingDir = "/Users/boppanavenkatesh/Desktop/dimensions";
+        String xmlFileName = "/DM Instance.xml";
+        Dimproc.GenerateDTs(workingDir, xmlFileName);
     }
 }
