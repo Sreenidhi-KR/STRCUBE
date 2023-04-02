a9840902ffc03e6c4ef04d8d3b1f84ae15f7ba54
BOPPANA VENKATESH <boppanavenkatesh97@gmail.com>
2023-04-02 23:17:35 +0530

    Updated SampleStream.java

--- src/main/java/org/example/SampleStream.java
+++ src/main/java/org/example/SampleStream.java
@@ -2,29 +2,33 @@ package org.example;
 
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SampleStream {
-    String url = "jdbc:mysql://localhost:3306/DataModeling?sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false&createDatabaseIfNotExist=true";
-    String username = "root"; // replace with your username
-    String password = "KVrsmck@21";
-
-    String dimensionsDirectory = "dimensions/";
-    String factsDirectory = "./facts/";
-    String xmlFileName = "DMInstance.xml";
-
+    public static String url = "jdbc:mysql://localhost:3306/DataModeling?sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false&createDatabaseIfNotExist=true";
+    public static String username = "root"; // replace with your username
+    public static String password = "KVrsmck@21";
     public static int Offset = 0;
-    public static int Size = 3;
-    public static int Velocity = 5;
+
+    public static int Size = 5;
+    public static int Velocity = 3;
     public static String csvFile = "./facts/stream.csv";
+
+    public static  String[] columnTypes = {"VARCHAR(255)","VARCHAR(255)","VARCHAR(255)","VARCHAR(255)","DECIMAL","DECIMAL","DECIMAL"};
     public static int TimeClick = 5000;
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, SQLException {
+        // Setup Connection
+        Connection conn = DriverManager.getConnection(url,username,password);
 
         BufferedReader reader = new BufferedReader(new FileReader(csvFile));
         String header = reader.readLine();
@@ -40,16 +44,21 @@ public class SampleStream {
             @Override
             public void run() {
                 try {
-                    SlidingWindow();
-                } catch (IOException e) {
+                    SlidingWindow(conn, header);
+                } catch (IOException | SQLException e) {
                     throw new RuntimeException(e);
                 }
             }
         }, 0, TimeClick);
 
     }
-    public static void SlidingWindow() throws IOException {
+    public static void SlidingWindow(Connection conn, String header) throws IOException, SQLException {
         System.out.println("...............................");
+        System.out.println("[Clearing fact table...]");
+        Statement statement = conn.createStatement();
+        String truncateFactTableQuery = "TRUNCATE TABLE FactTable";
+        statement.executeUpdate(truncateFactTableQuery);
+
         FileReader fileReader1 = new FileReader(csvFile);
         fileReader1.skip(Offset+1);
         FileReader fileReader2 = new FileReader(csvFile);
@@ -59,14 +68,30 @@ public class SampleStream {
         int temp = Velocity, window = Size;
         while (window > 0){
             String csvLine = S.readLine();
-            byte[] lineBytes = csvLine.getBytes(StandardCharsets.UTF_8);
-            System.out.println(csvLine);
+            String[] values = csvLine.split(",");
+            String insertSql = "INSERT INTO FactTable (" + header;
+            insertSql += ") VALUES (";
+            for (int i = 0; i < values.length; i++) {
+                if (columnTypes[i].equals("VARCHAR(255)")) {
+                    insertSql += "'" + values[i] + "'";
+                } else {
+                    insertSql += values[i];
+                }
+                if (i < values.length - 1) {
+                    insertSql += ", ";
+                }
+            }
+            insertSql += ")";
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(insertSql);
+            System.out.println(insertSql+" Executed Successfully...");
             --window;
         }
         while (!(S.readLine() == null) && temp > 0){
             --temp;
         }
         int avail = Velocity - temp;
+
         for(int i = 0 ; i < avail ; i++){
             String csvLine = R.readLine();
             byte[] lineBytes = csvLine.getBytes(StandardCharsets.UTF_8);
