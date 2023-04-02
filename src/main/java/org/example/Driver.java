28dd10210ecab9ac5eeb57a7c3f1100f10bc059f
BOPPANA VENKATESH <boppanavenkatesh97@gmail.com>
2023-04-03 01:47:24 +0530

    Added NewStreamProcessing.java

--- src/main/java/org/example/Driver.java
+++ src/main/java/org/example/Driver.java
@@ -4,13 +4,16 @@ public class Driver {
     public static void main(String[] args) throws Exception {
 //        DimensionalProcessing Dimproc = new DimensionalProcessing();
 //        Dimproc.CreateMetaDT();
-        String dimensionsDirectory = "dimensions/";
+        String dimensionsDirectory = "./dimensions/";
         String factsDirectory = "./facts/";
         String xmlFileName = "DMInstance.xml";
 //        Dimproc.GenerateDTs(dimensionsDirectory, xmlFileName);
 //        FactTableProcessing factTableProcessing=new FactTableProcessing();
 //        factTableProcessing.GenerateFT(dimensionsDirectory,xmlFileName);
-        StreamProcessing streamProcessing = new StreamProcessing();
-        streamProcessing.start(factsDirectory,dimensionsDirectory,xmlFileName);
+//        StreamProcessing streamProcessing = new StreamProcessing();
+//        streamProcessing.start(factsDirectory,dimensionsDirectory,xmlFileName);
+        /* Sliding Window Implementation... */
+        NewStreamProcessing nsp = new NewStreamProcessing();
+        nsp.startStreamService(factsDirectory, dimensionsDirectory, xmlFileName);
     }
 }
--- src/main/java/org/example/NewStreamProcessing.java
+++ src/main/java/org/example/NewStreamProcessing.java
@@ -1,5 +1,171 @@
 package org.example;
 
-public class NewStreamProcessing {
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Timer;
+import java.util.TimerTask;
+public class NewStreamProcessing {
+    // Database Credentials
+   public static String url = "jdbc:mysql://localhost:3306/DataModeling?sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false&createDatabaseIfNotExist=true";
+    public static String username = "root";
+    public static String password = "KVrsmck@21";
+    /* Offset: Stores Number of Bytes to Skip to get to Start of Window... */
+    public static int Offset = 0;
+
+    public NewStreamProcessing() throws FileNotFoundException {
+    }
+
+    public static void startStreamService(String factsDir ,String dimensionDir, String xmlFileName) throws SQLException, IOException {
+        int Size = 0;
+        int Velocity = 0;
+        int TimeClick = 0;
+        String csvPath = "";
+        File csvFile = null;
+        String[] columnTypes = null;
+        // Process XML Instance & Get DataSource Properties
+        try {
+            File inputFile = new File(dimensionDir+xmlFileName);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList dataSourceList = doc.getElementsByTagName("DataSource");
+            for (int i = 0; i < dataSourceList.getLength(); i++) {
+                Element dataSource = (Element) dataSourceList.item(i);
+                // Getting Column Types of Fact Table from XML...
+                NodeList DimensionVariableKeysList =  dataSource.getElementsByTagName("DimensionVariableKey");
+                NodeList FactVariablesList =  dataSource.getElementsByTagName("FactVariable");
+                columnTypes = new String[DimensionVariableKeysList.getLength()+FactVariablesList.getLength()];
+                int ind = 0;
+                for(; ind < DimensionVariableKeysList.getLength(); ind++){
+                    Element DimensionVariableKey = (Element) DimensionVariableKeysList.item(ind);
+                    columnTypes[ind] = DimensionVariableKey.getAttribute("type");
+                }
+                for(int j=0 ; j < FactVariablesList.getLength(); j++){
+                    Element FactVariable = (Element) FactVariablesList.item(j);
+                    columnTypes[ind++] = FactVariable.getAttribute("type");
+                }
+                // Getting Window Properties...
+                String size = dataSource.getElementsByTagName("size").item(0).getTextContent();
+                String sizeUnits = dataSource.getElementsByTagName("size").item(0).getAttributes().getNamedItem("units").getTextContent();
+                String time = dataSource.getElementsByTagName("time").item(0).getTextContent();
+                String timeUnits = dataSource.getElementsByTagName("time").item(0).getAttributes().getNamedItem("units").getTextContent();
+                String filePath = dataSource.getElementsByTagName("FilePath").item(0).getTextContent();
+                String filetype = dataSource.getElementsByTagName("FileType").item(0).getTextContent();
+                String velocity = dataSource.getElementsByTagName("velocity").item(0).getTextContent();
+                String velocityUnits = dataSource.getElementsByTagName("velocity").item(0).getAttributes().getNamedItem("units").getTextContent();
+
+
+                Size = Integer.parseInt(size);
+                Velocity = Integer.parseInt(velocity);
+                System.out.println("Size : " + size + " " + sizeUnits);
+                System.out.println("Clock Tick : " + time + " " + timeUnits);
+                System.out.println("Velocity : " + velocity + " " + velocityUnits);
+
+                if(timeUnits.contentEquals("seconds")){
+                    TimeClick = Integer.parseInt(time) * 1000;
+                }
+                else{
+                    TimeClick = Integer.parseInt(time);
+                }
+                System.out.println("File Path : " + filePath);
+
+                csvPath = factsDir.concat(filePath.concat(filetype));
+                csvFile = new File(csvPath);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Setup  Database Connection
+        Connection conn = DriverManager.getConnection(url,username,password);
+        // Read Header of csv File into 'header'...
+        BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+        String header = reader.readLine();
+        byte[] bytes = header.getBytes(StandardCharsets.UTF_8);
+        //System.out.println("Bytes Read" + bytes.length);
+        Offset += bytes.length;
+        //System.out.println(header);
+        reader.close();
+        // Setting Timer Configuration & Adding Method Call to Sliding Window...
+        Timer timer = new Timer();
+        File finalCsvFile = csvFile;
+        int finalVelocity = Velocity;
+        int finalSize = Size;
+        String[] finalColumnTypes = columnTypes;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    SlidingWindow(conn, header, finalCsvFile, finalVelocity, finalSize, finalColumnTypes);
+                } catch (IOException | SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, 0, TimeClick);
+    }
+    /* Sliding Window Method Definition... */
+    public static void SlidingWindow(Connection conn, String header, File csvFile, int Velocity, int Size, String[] columnTypes) throws IOException, SQLException {
+        System.out.println("...............................");
+        System.out.println("[Clearing fact table...]");
+        // Preparing to Truncate Table: FactTable (Simply Drops and Creates)
+        Statement statement = conn.createStatement();
+        String truncateFactTableQuery = "TRUNCATE TABLE FactTable";
+        statement.executeUpdate(truncateFactTableQuery);
+        System.out.println("[Fact table Truncated Successfully...]");
+        // Processing csv File
+        System.out.println("[Adding Stream Data...]");
+        FileReader fileReader1 = new FileReader(csvFile);
+        fileReader1.skip(Offset+1);
+        FileReader fileReader2 = new FileReader(csvFile);
+        fileReader2.skip(Offset+1);
+        // For Holding Start of Window
+        BufferedReader R = new BufferedReader(fileReader1);
+        // For Counting Next Available Window
+        BufferedReader S = new BufferedReader(fileReader2);
+        int temp = Velocity, window = Size;
+        while (window > 0){
+            String csvLine = S.readLine();
+            String[] values = csvLine.split(",");
+            // Inserting Window Data: Row by Row...
+            String insertSql = "INSERT INTO FactTable (" + header;
+            insertSql += ") VALUES (";
+            for (int i = 0; i < values.length; i++) {
+                if (columnTypes[i].equals("VARCHAR(255)")) {
+                    insertSql += "'" + values[i] + "'";
+                } else {
+                    insertSql += values[i];
+                }
+                if (i < values.length - 1) {
+                    insertSql += ", ";
+                }
+            }
+            insertSql += ")";
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(insertSql);
+            System.out.println(insertSql+" Executed Successfully...");
+            --window;
+        }
+        while (!(S.readLine() == null) && temp > 0){
+            --temp;
+        }
+        // Evaluate Available Lines of Data...
+        int avail = Velocity - temp;
+        // Now, Move Window @[(Velocity >= avail) ? Velocity : avail]
+        for(int i = 0 ; i < avail ; i++){
+            String csvLine = R.readLine();
+            byte[] lineBytes = csvLine.getBytes(StandardCharsets.UTF_8);
+            Offset += lineBytes.length + 1;
+        }
+    }
 }
