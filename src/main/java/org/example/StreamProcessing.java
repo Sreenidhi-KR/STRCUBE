84ea4e04583b1c59661a74aacc434efe9398a81b
Sreenidhi <rsri1998@gmail.com>
2023-04-03 15:49:39 +0530

    ability to add rows from skipped rows

--- src/main/java/org/example/StreamProcessing.java
+++ src/main/java/org/example/StreamProcessing.java
@@ -23,9 +23,13 @@ public class StreamProcessing {
         int windowClockTickInMillis = 0;
         String csvPath;
         File csvFile = null;
-        long lastLineOffsetNew = 0;
+        long lastLineOffset = 0;
+        long skippedLineOffset = 0;
         boolean initialRead = true;
         boolean earlySleep = false;
+        int noOfLinesReadInCurrentWindow = 0;
+        int noOfSkippedLinesInCurrentWindow = 0;
+        boolean isFirstSkippedRow ;
 
 
         String url = "jdbc:mysql://localhost:3306/DataModeling?sessionVariables=sql_mode='NO_ENGINE_SUBSTITUTION'&jdbcCompliantTruncation=false&createDatabaseIfNotExist=true";
@@ -81,7 +85,7 @@ public class StreamProcessing {
         String header = reader.readLine();
         byte[] bytes = header.getBytes(StandardCharsets.UTF_8);
         System.out.println("Bytes Read" + bytes.length);
-        lastLineOffsetNew += bytes.length;
+        lastLineOffset += bytes.length;
         System.out.println(header);
         String[] columnNames = header.split(",");
         reader.close();
@@ -93,20 +97,25 @@ public class StreamProcessing {
 
 
 
-        String insertQuery = "INSERT INTO FactTable (" + header + ") VALUES (" + String.join(",", Collections.nCopies(columnNames.length, "?")) + ")"; PreparedStatement insertStatement = conn.prepareStatement(insertQuery);
+        String insertQuery = "INSERT INTO FactTable (" + header + ") VALUES (" + String.join(",", Collections.nCopies(columnNames.length, "?")) + ")";
+        PreparedStatement insertStatement = conn.prepareStatement(insertQuery);
+        PreparedStatement insertSkippedStatement = conn.prepareStatement(insertQuery);
         PreparedStatement deleteNRowsQuery = conn.prepareStatement("DELETE FROM FactTable LIMIT ?");
 
 
         while (true) {
 
+            System.out.println("NEW WINDOW");
             System.out.println("CHECKING FOR NEW FACTS");
             FileReader fileReader = new FileReader(csvFile);
-            fileReader.skip(lastLineOffsetNew);
+
+            fileReader.skip(lastLineOffset);
 
             BufferedReader br = new BufferedReader(fileReader);
             String line;
-            int noOfLinesReadInCurrentWindow = 0;
-            int noOfSkippedLinesInCurrentWindow = 0;
+            noOfLinesReadInCurrentWindow = 0;
+            noOfSkippedLinesInCurrentWindow = 0;
+            isFirstSkippedRow = true;
 
 
             while (true) {
@@ -121,11 +130,18 @@ public class StreamProcessing {
                 }
 
                 bytes = line.getBytes(StandardCharsets.UTF_8);
-                lastLineOffsetNew += bytes.length +1;
+                long prevLineOffset = lastLineOffset;
+                lastLineOffset += bytes.length +1;
                 String[] values = line.split(",");
 
                 //Checking if we need to skip rows
                 if(!initialRead && ((windowSize-windowVelocity + noOfSkippedLinesInCurrentWindow) <0)){
+
+                    if(isFirstSkippedRow) {
+                        skippedLineOffset = prevLineOffset;
+                        isFirstSkippedRow = false;
+                    }
+
                     noOfSkippedLinesInCurrentWindow++;
                     continue;
                 }
@@ -153,20 +169,53 @@ public class StreamProcessing {
                 }
             }
 
+            if(noOfSkippedLinesInCurrentWindow > 0 && noOfLinesReadInCurrentWindow<windowSize){
+
+                System.out.println("INSERTING "+ (windowSize-noOfLinesReadInCurrentWindow) + " ROWS FROM SKIPPED ROWS");
+                FileReader tempFileReader = new FileReader(csvFile);
+                System.out.println(skippedLineOffset+1);
+                tempFileReader.skip(skippedLineOffset+1);
+                BufferedReader tempBr = new BufferedReader(tempFileReader);
+                int temp =  windowSize-noOfLinesReadInCurrentWindow;
+                for(int i =1 ; i<= temp ; i++){
+                    line = tempBr.readLine();
+                    if(line == null || line.trim().isEmpty()){
+                        break;
+                    }
+                    bytes = line.getBytes(StandardCharsets.UTF_8);
+                    skippedLineOffset += bytes.length +1;
+                    String[] values = line.split(",");
+                    for (int j = 0; j < columnNames.length; j++) {
+                        insertSkippedStatement.setString(j + 1, values[j]);
+                    }
+                    insertSkippedStatement.addBatch();
+                    noOfLinesReadInCurrentWindow++;
+                    noOfSkippedLinesInCurrentWindow--;
+                    System.out.println(insertSkippedStatement.toString());
+
+                }
+                tempBr.close();
+                tempFileReader.close();
+
+            }
+
 
             deleteNRowsQuery.setInt(1, noOfLinesReadInCurrentWindow);
             int rowsDeleted = deleteNRowsQuery.executeUpdate();
             System.out.println("FIRST " + rowsDeleted + " ROW DELETED FROM FACT TABLE");
 
 
+            int[] skippedUpdatesCounts = insertSkippedStatement.executeBatch();
+
             int[] updateCounts = insertStatement.executeBatch();
             System.out.println("INSERTED "+ updateCounts.length + " ROWS INTO FACT TABLE");
 
-
             if(noOfSkippedLinesInCurrentWindow > 0){
                 System.out.println("noOfSkippedLinesInCurrentWindow " + noOfSkippedLinesInCurrentWindow);
+                if(updateCounts.length>0) {
+                    System.out.println("INSERTED " + skippedUpdatesCounts.length + " SKIPPED ROWS INTO FACT TABLE");
+                }
             }
-
 
 
             br.close();
